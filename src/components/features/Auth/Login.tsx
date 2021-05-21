d5166152a683e4e261236d5c728a03f650e45143
Pharmman <pharm.sale777@gmail.com>
2021-05-21 17:02:20 +0300

    quantity characters for login password

--- src/components/features/Auth/Login.tsx
+++ src/components/features/Auth/Login.tsx
@@ -33,8 +33,8 @@ const validate = (values: InitialValuesType) => {
 
     if (!values.password) {
         errors.password = 'Required';
-    } else if (values.password.length < 6) {
-        errors.password = 'Must be 6 characters or more';
+    } else if (values.password.length < 4) {
+        errors.password = 'Must be 4 characters or more';
     }
     return errors
 }
